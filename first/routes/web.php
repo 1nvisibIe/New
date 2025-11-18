1d5c794e4b15eae7bfc9ce5fdf3c336661e49ac4
Ilya <sibaviator4@gmail.com>
2025-11-18 14:18:22 +0700

    route is working

--- first/routes/web.php
+++ first/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\MyFirstController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [MyFirstController::class, 'index']);
+
