c0e015590359acbb659110c51a5bb0313010ee06
Ilya <sibaviator4@gmail.com>
2025-11-26 17:07:48 +0700

    main

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\MyFirstController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [MyFirstController::class, 'index']);
+
+Route::get('/home/gg', [MyFirstController::class, 'index']);
