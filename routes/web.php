ed8612a720ad8ebe015fbe0549b08afb3d85db2f
IlyaLaptoop <sibaviator4444.com>
2025-11-27 11:27:56 +0700

    delete route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@ Route::get('/home', [MyFirstController::class, 'index']);
 
 Route::get('/home/gg', [MyFirstController::class, 'index']);
 
-Route::get('/home/gg/g', [MyFirstController::class, 'index']);
 
-Route::get('/home/gg/g/g', [MyFirstController::class, 'index']);
