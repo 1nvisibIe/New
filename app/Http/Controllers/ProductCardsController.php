b709c04709a92de9b623e5ad3afa444dfea2ac23
Ilya <sibaviator4@gmail.com>
2025-12-06 00:03:40 +0700

    New files

---
+++ app/Http/Controllers/ProductCardsController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use JetBrains\PhpStorm\NoReturn;
+
+class ProductCardsController extends Controller
+{
+
+    public function __construct(Request $request)
+    {
+       dump($request->route()->getName());
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view("ProductCards.index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("ProductCards.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+
+    public function store(Request $request)
+    {
+        dd($request);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return ("ProductCards $id");
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view("ProductCards.edit",["id"=>$id]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        dump($id);
+        dd($request);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
