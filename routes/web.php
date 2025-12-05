78d44fdbc8021d631e643c10bf0acddee968acf0
Ilya <sibaviator4@gmail.com>
2025-12-06 00:00:59 +0700

    Admin bez anim

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,38 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\MyFirstController;
+use App\Http\Controllers\ProductCardsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('HomePage');
 
 Route::get('/home', [MyFirstController::class, 'index']);
 
-Route::get('/home/gg', [MyFirstController::class, 'index']);
+Route::match(['GET', 'POST'], '/contact', function () {
+    if (!empty($_POST)) {
+        dump($_GET);
+        dump($_POST);
+    }
+    return view('contact');
+});
+//
+//Route::get('/contact', function (){
+//    return view('contact');
+//});
+//
+//Route::post('/send-email', function (){
+//    dump($_POST);
+//    return "Send email";
+//});
+//Route::redirect('/contact', '/HomePage',301);
 
+Route::fallback(function () {
+    return redirect()->route('HomePage');
+});
 
+Route::resource('ProductCards', ProductCardsController::class);
+
+Route::get('/admin',[MainController::class,'index2']);
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [ 'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+                'resources/assets/admin/css/adminlte.min.css',
+                'resources/assets/admin/plugins/jquery/jquery.min.js',
+                'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+                'resources/assets/admin/js/adminlte.min.js',
+                'resources/assets/admin/js/demo.js'
+
+                ],
             refresh: true,
         }),
         tailwindcss(),
